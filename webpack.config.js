d3900c3b503c7ce7ff467c9bd5b4cbc12bb0f10e
Andrés Ladino <122582963+ladinofel@users.noreply.github.com>
2023-11-29 15:56:12 -0500

    Establish the initial working files with webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode:"development",
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+      inject: 'body',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
