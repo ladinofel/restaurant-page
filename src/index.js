709a5c622178c1e8326885642d8f03c268421899
Andrés Ladino <122582963+ladinofel@users.noreply.github.com>
2023-11-30 15:13:27 -0500

    Implemented webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 console.log("This is working");
+console.log("Server Test");
--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode:"development",
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './src',
+  },
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
